4c7139fc120769adea938a1fe46e38ac7c9a4246
Johanna Bayer <hannabayer.89@gmail.com>
2024-12-02 09:58:26 +0100

    Update cherry_pick.sh

--- cherry_pick.sh
+++ cherry_pick.sh
@@ -4,24 +4,24 @@ mkdir cherry-pick-demo
 cd cherry-pick-demo
 
 # Create an initial commit on the main branch
-echo "Initial content" > file.txt
+echo "Initial file" > file.txt
 git add file.txt
-git commit -m "Add file file.txt on main"
+git commit -m "1. Add file file.txt on main"
 
 # Create a new branch for a feature
-git checkout -b feature
+git checkout -b cherry_feature
 
 # Make some changes in the feature branch
 echo "Feature content" >> file.txt
-git commit -am "Add feature content"
+git commit -am "2. Add feature content on cherry_feature"
 
 # Add a bug fix in the feature branch
 echo "Bug fix" >> file.txt
-git commit -am "Fix critical bug"
+git commit -am "3. Fix critical bug on cherry feature"
 
 # Add more changes in the feature branch
 echo "More feature content" >> file.txt
-git commit -am "Add more feature content"
+git commit -am " 4. Add more feature content on cherry_feature"
 
 # Switch back to the main branch
 git checkout main
