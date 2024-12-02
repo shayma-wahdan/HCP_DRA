0aef1c65e145143b7b18839e6e130e186ed41016
Johanna Bayer <hannabayer.89@gmail.com>
2024-12-02 09:59:30 +0100

    Update merge_rebase_ff.sh

--- merge_rebase_ff.sh
+++ merge_rebase_ff.sh
@@ -4,20 +4,20 @@ mkdir merge_example
 cd merge_example
 
 # Create an initial commit on main
-echo "Initial content" > file.txt
+echo "1. Initial content" > file.txt
 git add file.txt
-git commit -m "Initial commit on main"
+git commit -m "1. File on main"
 
 # Create a feature branch
 git checkout -b merge_feature
 
 # Make some commits in feature
-echo "Feature content 1" >> file.txt
-git commit -am "Add feature content 1"
-echo "Feature content 2" >> file.txt
-git commit -am "Add feature content 2"
+echo  "2. Feature content 1" >> file.txt
+git commit -am "2. Add feature content 1"
+echo "3. Feature content 2" >> file.txt
+git commit -am "3. Add feature content 2"
 
 # Switch back to main and make a commit
 git checkout main
-echo "Main branch content" >> file.txt
-git commit -am "Add main branch content"
+echo "4. Main branch content" >> file.txt
+git commit -am "4. Add main branch content"
